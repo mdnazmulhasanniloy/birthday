a86f8f12ab3965c4417ec278fa80bd0a66ebe0cd
MD Nazmul Hasan <105441794+mdnazmulhasanniloy@users.noreply.github.com>
2022-08-09 13:15:56 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Agu 03, 2022',
+  birthdate: 'Agu 10, 2022',
   name: 'Farhana'
 };
 
