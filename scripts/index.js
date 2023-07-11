7f2f1e4da7bccbdb7f5ca8b482f67d26e1f3a25e
MD Nazmul Hasan <105441794+mdnazmulhasanniloy@users.noreply.github.com>
2023-07-12 00:59:04 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Agu 10, 2022',
-  name: 'Sanjida'
+  birthdate: 'Jul 12, 2023',
+  name: 'Khadiza Mitu'
 };
 
 function hideEverything() {
